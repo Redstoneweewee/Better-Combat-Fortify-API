07d4e18859791246167711ce2390084792201852
Leifeng Chen <leifengchen@ucsb.edu>
2026-01-19 19:33:56 -0800

    fix: gitignore file updated and removed development files

---
+++ ts-setup/lib/Regolith/packs/ts-scripts/ScriptBox.js
@@ -0,0 +1,6 @@
+import { world } from "@minecraft/server";
+// When running a world with this behavior pack, type '/scriptevent sample:run' in chat to trigger this code.
+export function scriptBox(log, targetLocation) {
+    world.sendMessage("Hello world! Replace this line with your code.");
+}
+//# sourceMappingURL=ScriptBox.js.map
---
+++ ts-setup/lib/Regolith/packs/ts-scripts/main.js
@@ -0,0 +1,7 @@
+import SampleManager from "./SampleManager";
+import { scriptBox } from "./ScriptBox";
+const sm = new SampleManager();
+sm.registerSamples({
+    scriptBox: [scriptBox],
+});
+//# sourceMappingURL=main.js.map
